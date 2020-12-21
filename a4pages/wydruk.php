93011bbbd11e54a057c8e03109f8bfe781537a71
Szelbi <reven4on@gmail.com>
2020-12-21 23:03:38 +0100

    Initial commit

---
+++ a4pages/wydruk.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Strony A4</title>
+    <link rel="stylesheet" href="/loremipsum/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/loremipsum/css/customStyle.css">
+    <script src="/loremipsum/js/jquery-3.5.1.min.js"></script>
+    <script src="/loremipsum/js/bootstrap.min.js"></script>
+    <script src="/loremipsum/js/customScripts.js"></script>
+
+
+    <style>
+        body {
+            background: rgb(204, 204, 204);
+        }
+
+        page {
+            background: white;
+            display: block;
+            margin: 30px auto 150px;
+
+            /*margin-bottom: 3cm;*/
+            box-shadow: 0 0 0.5cm rgba(0, 0, 0, 0.5);
+        }
+
+        page[size="A4"] {
+            width: 21cm;
+            height: 29.7cm;
+        }
+
+        page[size="A4"][layout="landscape"] {
+            width: 29.7cm;
+            height: 21cm;
+        }
+
+        page[size="A3"] {
+            width: 29.7cm;
+            height: 42cm;
+        }
+
+        page[size="A3"][layout="landscape"] {
+            width: 42cm;
+            height: 29.7cm;
+        }
+
+        page[size="A5"] {
+            width: 14.8cm;
+            height: 21cm;
+        }
+
+        page[size="A5"][layout="landscape"] {
+            width: 21cm;
+            height: 14.8cm;
+        }
+
+        @media print {
+            body, page {
+                margin: 0;
+                box-shadow: 0;
+            }
+        }
+
+        .content {
+            padding: 50px
+        }
+
+        object {
+            height: 100%;
+            width: 100%;
+        }
+
+        h1, h2, h3, h4 {
+            text-align: center;
+        }
+    </style>
+
+</head>
+
+<header>
+    <div id="nav-placeholder">
+    </div>
+</header>
+<body>
+
+<div>
+	<?php
+
+	$data = "/loremipsum/files/lorem.txt";
+	for ($i = 5; $i >= 3; $i--) {
+		echo '<h3>A' . $i . '</h3>';
+
+		$layouts = [NULL, 'layout="landscape"'];
+
+		foreach ($layouts as $layout) {
+			echo '  <page size="A' . $i . '" ' . $layout . '>
+                    <div class="content" style="height:100%;">
+                        <object data="/loremipsum/files/lorem.txt"></object>
+                    </div>
+                </page>';
+		}
+	}
+	echo '<div>123</div>';
+
+	?>
+
+
+</div>
+
+</body>
+
+<script>
+    $(function () {
+        $("#nav-placeholder").load("/loremipsum/navbar.html");
+    });
+</script>
+
+</html>
+
---
+++ colors.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Kolorki</title>
+    <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="/loremipsum/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/loremipsum/css/customStyle.css">
+    <script src="/loremipsum/js/jquery-3.5.1.min.js"></script>
+    <script src="/loremipsum/js/bootstrap.min.js"></script>
+
+    <style>
+        color-div {
+            height: 30px;
+
+        }
+
+        color-row {
+            float: left;
+        }
+        .link{
+            text-align: center;
+            font-size: 28px;
+            padding-bottom: 20px;
+        }
+    </style>
+</head>
+
+
+<header>
+    <div id="nav-placeholder">
+    </div>
+</header>
+
+
+<body>
+<main class="main">
+        <div class="container col-md-8">
+            <div class="link">
+                <a href="https://www.w3schools.com/cssref/func_hsl.asp">Kolorki HSL</a>
+            </div>
+			<?php
+
+			$lights = array(20, 40, 60, 80);
+			foreach ($lights as $light) {
+				echo '<div class="col-md-3 color-row" style="float:left">
+                  <h4>' . $light . '% jasności</h4>';
+
+				$colors = range(0, 360, 10);
+				foreach ($colors as $color) {
+					echo '<div class="color-div" style="background: hsl(' . $color . ',100%, ' . $light . '%)">' . $color . '</div>';
+
+				}
+				echo '</div>';
+			}
+			?>
+    </div>
+</main>
+
+</body>
+
+<script>
+    $(function () {
+        $("#nav-placeholder").load("navbar.html");
+    });
+</script>
+</html>
