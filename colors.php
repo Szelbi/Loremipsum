3706f14b90d9718e6a0e0d47488ae75b2aea51c6
Szelbi <reven4on@gmail.com>
2020-12-22 19:09:38 +0100

    code organization

--- colors.php
+++ colors.php
@@ -12,13 +12,13 @@
     <style>
         color-div {
             height: 30px;
-
         }
 
         color-row {
             float: left;
         }
-        .link{
+
+        .link {
             text-align: center;
             font-size: 28px;
             padding-bottom: 20px;
@@ -35,25 +35,25 @@
 
 <body>
 <main class="main">
-        <div class="container col-md-8">
-            <div class="link">
-                <a href="https://www.w3schools.com/cssref/func_hsl.asp">Kolorki HSL</a>
-            </div>
-			<?php
+    <div class="container col-md-8">
+        <div class="link">
+            <a href="https://www.w3schools.com/cssref/func_hsl.asp">Kolorki HSL</a>
+        </div>
+		<?php
 
-			$lights = array(20, 40, 60, 80);
-			foreach ($lights as $light) {
-				echo '<div class="col-md-3 color-row" style="float:left">
+		$lights = array(20, 40, 60, 80);
+		foreach ($lights as $light) {
+			echo '<div class="col-md-3 color-row" style="float:left">
                   <h4>' . $light . '% jasności</h4>';
 
-				$colors = range(0, 360, 10);
-				foreach ($colors as $color) {
-					echo '<div class="color-div" style="background: hsl(' . $color . ',100%, ' . $light . '%)">' . $color . '</div>';
+			$colors = range(0, 360, 10);
+			foreach ($colors as $color) {
+				echo '<div class="color-div" style="background: hsl(' . $color . ',100%, ' . $light . '%)">' . $color . '</div>';
 
-				}
-				echo '</div>';
 			}
-			?>
+			echo '</div>';
+		}
+		?>
     </div>
 </main>
 
