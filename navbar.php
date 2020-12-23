8435ae67cc8044f65e5e18da49b3f0c44ecc42c0
Szelbi <reven4on@gmail.com>
2020-12-23 01:09:01 +0100

    index.php

--- navbar.php
+++ navbar.php
@@ -9,7 +9,7 @@
 </style>
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" id="navbar">
-	<a class="navbar-brand" href="/loremipsum/index.html">Home</a>
+	<a class="navbar-brand" href="/loremipsum/index.php">Home</a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
 			aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
