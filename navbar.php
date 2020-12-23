c3a5bf04da4035982d5a576d0a06ec62ad4506db
Szelbi <reven4on@gmail.com>
2020-12-24 00:22:06 +0100

    removed .navbar-toggler button

--- navbar.php
+++ navbar.php
@@ -12,10 +12,6 @@
 
 <nav class="navbar navbar-expand-md navbar-dark bg-dark" id="navbar">
 	<a class="navbar-brand" href="/loremipsum/index.php">Home</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-			aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
 
 	<div class="collapse navbar-collapse">
 		<ul class="navbar-nav mr-auto">
