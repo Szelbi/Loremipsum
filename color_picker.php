1e890baf682016d01f463371604f1d8d3eb62efb
Szelbi <reven4on@gmail.com>
2021-01-01 12:51:53 +0100

    Tutaj docelowo będzie funkcjoanlność wybierania kolorków przy pomocy JavaScript

---
+++ color_picker.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Color picker</title>
+    <link rel="stylesheet" href="/loremipsum/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/loremipsum/css/customStyle.css">
+
+
+</head>
+
+<header>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/loremipsum/navbar.php') ?>
+</header>
+
+<body>
+<main class="main">
+
+    <h3>Footer test</h3>
+
+    <button type="button" id="button" onclick="myFunction()">Duplicate</button>
+
+
+    <p id="output">Lore ipsum dolor sit amet, consectetur adipisicing elit. Architecto assumenda aut debitis deserunt
+        dignissimos dolore dolorum ea fuga itaque iusto libero magni nam, nostrum obcaecati, quaerat quidem quis soluta
+        unde?</p>
+</main>
+</body>
+
+<footer>
+	<?php include($_SERVER['DOCUMENT_ROOT'] . '/loremipsum/footer.html') ?>
+</footer>
+</html>
