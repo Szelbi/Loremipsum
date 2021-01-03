636703a300ec1939521c26c2934a9a767a711542
Szelbi <reven4on@gmail.com>
2021-01-04 00:51:50 +0100

    Zmiana kolorków działa (na razie na mouseover)

--- colors/color_picker.js
+++ colors/color_picker.js
@@ -1,9 +1,92 @@
+/**
+ * Hue
+ */
+var slider_h = document.getElementById("hsl-h");
+var result_h = document.getElementById("hsl-h-result");
+result_h.value = slider_h.value;
 
-
-var slider = document.getElementById("hsl-h");
-var result = document.getElementById("hsl-h-result");
-result.value = slider.value;
-
-slider.oninput = function() {
-    result.value = this.value;
+slider_h.oninput = function() {
+    result_h.value = slider_h.value;
 }
+result_h.oninput = function() {
+    slider_h.value = result_h.value;
+}
+
+
+
+/**
+ * Saturation
+ */
+var slider_s = document.getElementById("hsl-s");
+var result_s = document.getElementById("hsl-s-result");
+result_s.value = slider_s.value;
+
+slider_s.oninput = function() {
+    result_s.value = slider_s.value;
+}
+result_s.oninput = function() {
+    slider_s.value = result_s.value;
+}
+
+
+/**
+ * Lightness
+ */
+var slider_l = document.getElementById("hsl-l");
+var result_l = document.getElementById("hsl-l-result");
+result_l.value = slider_l.value;
+
+slider_l.oninput = function() {
+    result_l.value = slider_l.value;
+}
+result_l.oninput = function() {
+    slider_l.value = result_l.value;
+}
+
+
+/**
+ * Alpha
+ */
+var slider_a = document.getElementById("hsl-a");
+var result_a = document.getElementById("hsl-a-result");
+result_a.value = slider_a.value;
+
+slider_a.oninput = function() {
+    result_a.value = slider_a.value;
+}
+result_a.oninput = function() {
+    slider_a.value = result_a.value;
+}
+
+//
+// document.querySelector('.slider').addEventListener('click', event ={
+//
+// })
+
+myFunction = function() {
+
+    var hue = slider_h.value;
+    var sat = slider_s.value;
+    var lig = slider_l.value;
+    var alpha = slider_a.value;
+    let results = "hsla(" + hue + ", " + sat + '%, ' + lig + '%, ' + alpha +")";
+    wynik.textContent = results;
+    document.getElementById("box").style.backgroundColor = results;
+}
+
+var sliders = document.getElementsByClassName("slider");
+
+for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener('click', myFunction);
+}
+
+for (var i = 0; i < sliders.length; i++) {
+    sliders[i].addEventListener('onmousemove', myFunction);
+}
+
+
+
+// function outputUpdate(vol) {
+//     document.querySelector('#hsl-s-result').value = (vol);
+//     // alert(-(vol - 100));
+// }
