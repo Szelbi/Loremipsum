01b6c69bd868556975ec947e56fde67f0955e623
Szelbi <reven4on@gmail.com>
2021-01-03 14:18:32 +0100

    Dodano sekcję sliderów, dodano klasy

--- colors/color_picker.php
+++ colors/color_picker.php
@@ -17,16 +17,28 @@
 
 
     <div class="container">
-        <div class="row" style="height: 500px">
+        <div class="row" style="height: 500px; background-color: #ddd">
             <div class="col-md-4 border-orange" id="outer">
                 <h4>Result</h4>
                 <div class="result-box center"></div>
             </div>
             <div class="col-md-8 border-orange">
-                <h4>Column 2</h4>
-                <div class="slider">Slider 1</div>
-                <div class="slider">2</div>
-                <div class="slider">3</div>
+                <h4>Sliders</h4>
+                <div class="picker">
+                    <p class="label" title="Hue">H</p>
+                    <input type="range" class="slider" id="hsl-h" min="1" max="360" value="180" aria-label="hue">
+                    <input type="text" class="result" id="hsl-h-result" aria-label="hue">
+                </div>
+                <p class="label" title="Saturation">S</p>
+                <div class="picker">
+                    <input type="range" class="slider" id="hsl-h" min="1" max="360" value="180" aria-label="hue">
+                    <input type="text" class="result" id="hsl-h-result" aria-label="hue">
+                </div>
+                <div class="picker">
+                    <p class="label" title="Lightness">L</p>
+                    <input type="range" class="slider" id="hsl-h" min="1" max="360" value="180" aria-label="hue">
+                    <input type="text" class="result" id="hsl-h-result" aria-label="hue">
+                </div>
             </div>
         </div>
     </div>
@@ -37,3 +49,5 @@
 	<?php include($_SERVER['DOCUMENT_ROOT'] . '/loremipsum/footer.html') ?>
 </footer>
 </html>
+
+<script src="/loremipsum/colors/color_picker.js"></script>
