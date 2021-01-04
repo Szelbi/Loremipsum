f595e8ccad7d983bfd30392bd0a78525586c19fe
Szelbi <reven4on@gmail.com>
2021-01-05 00:42:35 +0100

    Poprawiono działanie sliderów. Teraz działają już na oninput i zmieniają się dynamicznie podczas przesywania.

--- colors/color_picker.js
+++ colors/color_picker.js
@@ -5,13 +5,12 @@ let slider_h = document.getElementById("hsl-h");
 let result_h = document.getElementById("hsl-h-result");
 result_h.value = slider_h.value;
 
-slider_h.oninput = function() {
+slider_h.oninput = function () {
     result_h.value = slider_h.value;
 }
-result_h.oninput = function() {
+result_h.oninput = function () {
     slider_h.value = result_h.value;
 }
-
 
 
 /**
@@ -21,10 +20,10 @@ let slider_s = document.getElementById("hsl-s");
 let result_s = document.getElementById("hsl-s-result");
 result_s.value = slider_s.value;
 
-slider_s.oninput = function() {
+slider_s.oninput = function () {
     result_s.value = slider_s.value;
 }
-result_s.oninput = function() {
+result_s.oninput = function () {
     slider_s.value = result_s.value;
 }
 
@@ -36,10 +35,10 @@ let slider_l = document.getElementById("hsl-l");
 let result_l = document.getElementById("hsl-l-result");
 result_l.value = slider_l.value;
 
-slider_l.oninput = function() {
+slider_l.oninput = function () {
     result_l.value = slider_l.value;
 }
-result_l.oninput = function() {
+result_l.oninput = function () {
     slider_l.value = result_l.value;
 }
 
@@ -51,42 +50,47 @@ let slider_a = document.getElementById("hsl-a");
 let result_a = document.getElementById("hsl-a-result");
 result_a.value = slider_a.value;
 
-slider_a.oninput = function() {
+slider_a.oninput = function () {
     result_a.value = slider_a.value;
 }
-result_a.oninput = function() {
+result_a.oninput = function () {
     slider_a.value = result_a.value;
 }
 
-//
-// document.querySelector('.slider').addEventListener('click', event ={
-//
-// })
 
-myFunction = function() {
-
+colorMainBox = function () {
     let hue = slider_h.value;
     let sat = slider_s.value;
     let lig = slider_l.value;
     let alpha = slider_a.value;
-    let results = "hsla(" + hue + ", " + sat + '%, ' + lig + '%, ' + alpha +")";
-    wynik.textContent = results;
-    document.getElementById("box").style.backgroundColor = results;
+    let results = "hsla(" + hue + ", " + sat + '%, ' + lig + '%, ' + alpha + ")";
+    wynik.textContent = results; // Wartość dla <p> z wynikiem pod sliderami
+    document.getElementById("main-box").style.backgroundColor = results; // Kolor dla boxa głównego
 }
 
+/**
+ * range slidery .slider
+ */
 let sliders = document.getElementsByClassName("slider");
 for (let i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener('click', myFunction);
-}
-
-for (let i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener('input', myFunction);
+    sliders[i].addEventListener('input', colorMainBox);
 }
 
 
+/**
+ * inputy .result
+ */
+let results = document.getElementsByClassName("result");
+for (let i = 0; i < results.length; i++) {
 
-function outputUpdate(vol) {
-    document.querySelector('#hsl-s-result').value = (vol);
-    document.querySelector('#range').value = -(vol - 100);
-    // alert(-(vol - 100));
+    // Do wszystkich inputów result dodaję funkcję z automatycznym zaznaczaniem tekstu po kliknięciu
+    results[i].addEventListener('click', function () {
+        this.select();
+    })
+
+    // Dodaję funkcję colorMainBox przy wprowadzaniu wartości ręcznie do pola input
+    results[i].addEventListener('input', colorMainBox)
+
 }
+
+
