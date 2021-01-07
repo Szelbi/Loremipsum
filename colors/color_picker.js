5d8aa12cf03739a08671952beba6905785d18390
Szelbi <reven4on@gmail.com>
2021-01-07 23:56:25 +0100

    Created class hlsa. This class contains all 4 hsla properties. Every other functions uses properties of its object. Working fine after massive changes.

--- colors/color_picker.js
+++ colors/color_picker.js
@@ -1,23 +1,59 @@
+/**
+ *  Ustawia background slidera Hue na całą paletę HSL
+ *  Robię to w JS żeby dynamicznie wygenerować paletę zamiast pisać długiego CSS z ~12 kolorami
+ */
+setHueBgColor = function () {
+
+    let colors = [];
+    for (let i = 0; i <= 360; i += 10) {
+        let color = 'hsla(' + i + ', 100%, 50%, 1)';
+        colors.push(color);
+    }
+
+    let hslh = document.getElementById('hsl-h');
+    hslh.style.background = 'linear-gradient(90deg, ' + colors + ')';
+}
+setHueBgColor();
+
+
 
 
 /**
  * Pobieranie wartości HSLA na podstawie podanego id slidera
  */
-getColorValue = function(sliderId) {
+getSliderValue = function (sliderId) {
 
     let slider = document.getElementById(sliderId);
     let result = document.getElementById(sliderId + "-result");
 
     let value = slider.value;
-    result.value  = value;
+    result.value = value;
 
     slider.oninput = function () {
         result.value = slider.value;
     }
+    // result.oninput = function () {
+    //     slider.value = result.value;
+    // }
+
+    return value;
+}
+
+
+getResultValue = function (sliderId) {
+
+    let slider = document.getElementById(sliderId);
+    let result = document.getElementById(sliderId + "-result");
+
+    let value = result.value;
+    //result.value  = value;
+
+    // slider.oninput = function () {
+    //     result.value = slider.value;
+    // }
     result.oninput = function () {
-        slider.value = result.value;
+        slider.value = value;
     }
-    console.log(value);
 
     return value;
 }
@@ -27,12 +63,48 @@ getColorValue = function(sliderId) {
  * Obiekt 'hsla'. Zawiera wartości składowych HSLA
  * @type {{saturation: (*|string), lightness: (*|string), alpha: (*|string), hue: (*|string)}}
  */
-let hsla = {
-    hue: getColorValue("hsl-h"),
-    saturation: getColorValue("hsl-s"),
-    lightness: getColorValue("hsl-l"),
-    alpha: getColorValue("hsl-a")
+class hsla {
+
+    hue;
+    saturation;
+    lightness;
+    alpha;
+
+    constructor(hue, saturation, lightness, alpha) {
+        this.hue = hue;
+        this.saturation = saturation;
+        this.lightness = lightness;
+        this.alpha = alpha;
+    }
+
+    update(callback) {
+        this.hue = callback("hsl-h");
+        this.saturation = callback("hsl-s");
+        this.lightness = callback("hsl-l");
+        this.alpha = callback("hsl-a");
+        console.log(this.hue);
+    }
 }
+
+/**
+ * Ustawianie koloru main-boxa.
+ * Dodatkowo wstawiam wartość $result do lini wyniku #wynik
+ * @param obj
+ */
+setBoxColor = function (obj) {
+    let results = `hsla(${obj.hue}, ${obj.saturation}%,  ${obj.lightness}%,  ${obj.alpha})`;
+    console.log(results);
+    let wynik = document.getElementById("wynik");
+    wynik.textContent = results; // Wartość dla <p> z wynikiem pod sliderami
+    document.getElementById("main-box").style.backgroundColor = results; // Kolor dla boxa głównego
+}
+
+//TODO Wartości domyślne chyba mogłbyby być ładowane z konstruktora. Można to przerobić.
+let obj = new hsla(180, 100, 50, 1);
+
+obj.update(getSliderValue);
+obj.update(getResultValue);
+setBoxColor(obj);
 
 
 /**
@@ -40,23 +112,11 @@ let hsla = {
  */
 let sliders = document.getElementsByClassName("slider");
 for (let i = 0; i < sliders.length; i++) {
-    sliders[i].addEventListener('input', () => getColorValue(sliders[i].id));
-    console.log(sliders[i].id);
+    sliders[i].addEventListener('input', () => {
+        obj.update(getSliderValue);
+        setBoxColor(obj)
+    });
 }
-
-console.log(hsla);
-
-
-
-setColor = function () {
-    let results = "hsla(" + hsla.hue + ", " + hsla.saturation + '%, ' + hsla.lightness + '%, ' + hsla.alpha + ")";
-
-    let wynik = document.getElementById("wynik");
-    wynik.textContent = results; // Wartość dla <p> z wynikiem pod sliderami
-    document.getElementById("main-box").style.backgroundColor = results; // Kolor dla boxa głównego
-}
-
-setColor();
 
 
 /**
@@ -71,28 +131,11 @@ for (let i = 0; i < results.length; i++) {
     })
 //
     // Dodaję funkcję colorMainBox przy wprowadzaniu wartości ręcznie do pola input
-    results[i].addEventListener('input', setColor)
+    results[i].addEventListener('input', () => {
+        obj.update(getResultValue);
+        setBoxColor(obj)
+    });
 }
 
-
-
-
-/**
- *  Ustawia background slidera Hue na całą paletę HSL
- *  Robię to w JS żeby dynamicznie wygenerować paletę zamiast pisać długiego CSS z ~12 kolorami
- */
-setHueBgColor = function() {
-
-    let colors = [];
-    for(let i=0 ; i<=360; i+=10){
-        let color = 'hsla(' + i +', 100%, 50%, 1)';
-        colors.push(color);
-    }
-
-    let hslh = document.getElementById('hsl-h');
-    hslh.style.background = 'linear-gradient(90deg, ' + colors + ')';
-
-}
-setHueBgColor();
 
 
