21dc4bc7d65d7bb32739601a6135b2f15a8d8d94
Szelbi <reven4on@gmail.com>
2021-01-11 23:11:27 +0100

    Added comma to dot replace for hsl-a-result input

--- colors/color_picker.js
+++ colors/color_picker.js
@@ -1,4 +1,110 @@
 /**
+ * Zmieniam przecinek na kropnę w inpucie hsl-a-result
+ */
+document.getElementById('hsl-a-result').addEventListener('input', function() {
+    this.value = this.value.replace(/,/g, '.')
+    }
+);
+
+
+
+inputFilter = function(object, event){
+
+    let val = object.value;
+    let id = object.id;
+
+    console.log(val);
+    // console.log(id);
+    console.log(object);
+    console.log(event.data);
+    // console.log(event.key);
+
+
+
+    // if(elemId === "hsl-h-result")
+    //     console.log(elemId);
+    //
+    // if(elemId === "hsl-a-result")
+    //     console.log("a");
+
+    // let val = document.getElementById(elemId).value;
+    // console.log(val);
+    // if (val > 1000)
+    //     return '';
+
+}
+
+document.getElementById('hsl-s-result').addEventListener('input', function(e) {
+    inputFilter(this, e)
+    // console.log(e);
+    // console.log(this.value);
+}
+);
+
+
+
+let input = document.getElementById('hsl-l-result');
+input.addEventListener('keyup', function (event) {
+    console.log(event.which);
+    if (event.which !== 8 && event.which !== 0 && event.which < 48 || event.which > 57) {
+        console.log(event);
+        this.value = this.value.replace(/\D/g, "");
+        // return false;
+    }
+});
+
+// document.getElementById("hsl-h-result").addEventListener('input', function () {
+//         console.log(1);
+//     })
+
+
+// let results1 = document.getElementsByClassName("result");
+// for (let i = 0; i < results1.length; i++) {
+//     results1[i].addEventListener('input', () => {
+//         inputFilter(results1[i].id)
+//     });
+// }
+
+
+
+// Restricts input for the given textbox to the given inputFilter.
+// function setInputFilter(textbox, inputFilter) {
+//     ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+//         textbox.addEventListener(event, function() {
+//             if (inputFilter(this.value)) {
+//                 this.oldValue = this.value;
+//                 this.oldSelectionStart = this.selectionStart;
+//                 this.oldSelectionEnd = this.selectionEnd;
+//             } else if (this.hasOwnProperty("oldValue")) {
+//                 this.value = this.oldValue;
+//                 this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+//             } else {
+//                 this.value = "";
+//             }
+//         });
+//     });
+// }
+
+// Install input filters.
+// setInputFilter(document.getElementById("hsl-h-result"), function(value) {
+//     return /^-?\d*$/.test(value); });
+// setInputFilter(document.getElementById("uintTextBox"), function(value) {
+//     return /^\d*$/.test(value); });
+// setInputFilter(document.getElementById("intLimitTextBox"), function(value) {
+//     return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 500); });
+// setInputFilter(document.getElementById("floatTextBox"), function(value) {
+//     return /^-?\d*[.,]?\d*$/.test(value); });
+// setInputFilter(document.getElementById("currencyTextBox"), function(value) {
+//     return /^-?\d*[.,]?\d{0,2}$/.test(value); });
+// setInputFilter(document.getElementById("latinTextBox"), function(value) {
+//     return /^[a-z]*$/i.test(value); });
+// setInputFilter(document.getElementById("hexTextBox"), function(value) {
+//     return /^[0-9a-f]*$/i.test(value); });
+
+
+
+
+/**
  *  Ustawia background slidera Hue na całą paletę HSL
  *  Robię to w JS żeby dynamicznie wygenerować paletę zamiast pisać długiego CSS z ~12 kolorami
  */
@@ -92,12 +198,14 @@ setBoxColor = function (obj) {
     document.getElementById("main-box").style.backgroundColor = results; // Kolor dla boxa głównego
 }
 
+
 //TODO Wartości domyślne chyba mogłbyby być ładowane z konstruktora. Można to przerobić.
 let obj = new hsla(180, 100, 50, 1);
-
 obj.update(getSliderValue);
 obj.update(getResultValue);
 setBoxColor(obj);
+
+
 
 
 /**
@@ -122,18 +230,25 @@ for (let i = 0; i < results.length; i++) {
     results[i].addEventListener('click', function () {
         this.select();
     })
-//
+
+    // results[i].addEventListener('keypress', () => {
+        // inputFilter(results[i].id)
+    // });
+
     // Dodaję funkcję colorMainBox przy wprowadzaniu wartości ręcznie do pola input
     results[i].addEventListener('input', () => {
         obj.update(getResultValue);
         setBoxColor(obj)
     });
+    // Dodaję funkcję colorMainBox przy wprowadzaniu wartości ręcznie do pola input
+
 }
 
 
 
 /*
 TODO
-Jest jakiś problem podczas wpisywania result z ręki. Może % się gdzieś przekazuje?
 Dodać walidację pól result
  */
+
+
