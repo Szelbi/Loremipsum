eff3f043f81aaa744f49001f6466f01a19ce1de4
Szelbi <reven4on@gmail.com>
2021-01-25 00:14:01 +0100

    Added array changing for button innerHTML

--- basics/basic.js
+++ basics/basic.js
@@ -1,8 +1,11 @@
-function hideDiv(elem) {
+function hideDiv(elem, obj) {
     let x = document.getElementById(elem);
     if (x.style.display === "block") {
         x.style.display = "none";
+        obj.innerHTML = "►"
     } else {
         x.style.display = "block";
+        obj.innerHTML = "▼"
     }
+    console.log(obj);
 }
